1479a3067db0fe68acf4895a62c73ea5f13584f5
Andy Watt <andy.watt83@gmail.com>
2019-01-24 00:10:40 +0000

    working example using uPort to log in

---
+++ src/util/connectors.js
@@ -0,0 +1,12 @@
+import { Connect, SimpleSigner } from 'uport-connect'
+
+//export let uport = new Connect('TruffleBox')
+
+export const uport = new Connect('AKW React Uport box', {
+    clientId: '2oeNXcXuNmV3oURLpkHkiGYggZRwpaSJxXn',
+    network: 'rinkeby',
+    signer: SimpleSigner('fdc0b97e3536c9a4f3320e4b4429989ba7e9fa5dccb0d22b365b76eb9245f5d0')
+    })
+
+export const web3 = uport.getWeb3()
+
